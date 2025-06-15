0984423e31962444e206ea59431b6c338dd79da3
RAGHAW PATHAK <raghawpathak001@gmail.com>
2025-06-15 18:33:24 +0530

    Practices Some Basic Question Of JavaScript

---
+++ 01_Basics/11_Practice.js
@@ -0,0 +1,94 @@
+// Q1
+
+// let fullname = "Raghaw Pathak"
+// let age = 18
+// let isbool = true
+
+// console.log(`Hi, my name is ${fullname}. I am ${age} years old. Student: ${isbool}`);
+
+
+// Q2
+
+// let city = "Delhi";
+// let pinCode = 110001;
+// let isCapital = true;
+// let population = null;
+// let undefinedData;
+
+// console.log(`Type of city is:`, typeof (city));
+// console.log(`Type of pinCode is:`, typeof (pinCode));
+// console.log(`Type of isCapital is:`, typeof (isCapital));
+// console.log(`Type of population is:`, typeof (population));
+// console.log(`Type of undefinedData is:`, typeof (undefinedData));
+
+
+// Q3
+
+// let message = "   JavaScript is Awesome!   ";
+
+// console.log(message.trim());
+// console.log(message.toLowerCase());
+// console.log(message.includes("JavaScript"));
+// console.log(message.replace("Awesome", "Powerfull"));
+
+// Q4
+
+// let tags = "js,html,css,react,node";
+
+// arrtag = tags.split(",");
+// arrtag.push("mongodb");
+// console.log(arrtag.join("|"));
+
+// Q5
+
+// let sentence = "Learning JavaScript is fun and powerful!";
+
+// console.log(sentence.indexOf("fun"));
+// console.log(sentence.substring(9, 19));
+// console.log(sentence.slice(9, 19));
+
+// Q6
+
+// let num = 5.6789;
+
+// console.log(num.toFixed(2));
+
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+// Q7
+
+let strNum = "123.45";
+
+// num = new Number(strNum)
+// console.log(num);
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// againStrNum = new String(num)
+// console.log(againStrNum);
+
+
+// Q8
+
+// let currentDate = new Date();
+
+// console.log(currentDate);
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getDate());
+// console.log(currentDate.toDateString());
+
+// Q9
+
+let date = new Date("2025-06-15T10:30:00");
+
+console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMinutes());
+
+// Q10
+
